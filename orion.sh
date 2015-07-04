e99abbc6be8a3c54f6f23cef2c0ec3a32b17cf2d
SohamJ <soham4jam@gmail.com>
2015-07-04 15:00:31 -0400

    generalize build script for all devices

--- orion.sh
+++ orion.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
-echo '1:repo sync and build, 2:build'
-read choice
-if [ "$choice" == "1" ]; then
-	eval "cd ~/orion"
-	eval "repo sync -j16"
-elif [ "$choice" == "2" ]; then
-	eval "cd ~/orion"
+# Give command line argument for the device ./orion.sh <device>
+
+if [ "$1" != "" ]; then
+	echo '1:repo sync and build, 2:build'
+	read choice
+	if [ "$choice" == "1" ]; then
+		eval "cd ~/orion"
+		eval "repo sync -j16"
+	elif [ "$choice" == "2" ]; then
+		eval "cd ~/orion"
+	fi
+		eval ". build/envsetup.sh"
+		eval "export USE_PREBUILT_CHROMIUM=1"
+		eval "brunch $1"
+else
+	echo "error: enter device name"
 fi
-	eval ". build/envsetup.sh"
-	eval "export USE_PREBUILT_CHROMIUM=1"
-	eval "brunch falcon"
