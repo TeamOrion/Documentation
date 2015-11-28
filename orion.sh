6549c1d3f954c0cdfe0f721badb71a8b3445235d
SohamJ <soham4jam@gmail.com>
2015-11-28 17:38:36 -0500

    Revamp build script
    Change-Id: I8d8c2268c2d46c3416e3cabd66a82c1b6a49116d

--- orion.sh
+++ orion.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 #=========================================
 #Build script for OrionOS
-#Copy or link this file from Documentation to the root folder or run it from documentation for the first time
-#To link, do ln -s Documentation/orion.sh . 
-#Give command line argument for the device bash orion.sh <flags> <device>. 
-#Kill the process using Control + C
 
-#=========================================
-
-#=========================================
-#ENVIRONMENT VARIABLES
-#Set your environment variables (some set by default)
-alias ORION_HOME="cd /home/`whoami`/orion"
-ORION_DEVICES="/home/`whoami`/orion/vendor/orion/orion.devices"
-#=========================================
+#Give command line argument for the device :  ./orion.sh <flags> <device>. 
 
 #=========================================
 #FLAGS - 
@@ -23,36 +12,53 @@ ORION_DEVICES="/home/`whoami`/orion/vendor/orion/orion.devices"
 # p = clear prebuilt chromium
 #=========================================
 
+#COLORS -
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+end=$'\e[0m'
 
-$ORION_HOME
+#=========================================
+
+#SET THE HOME DIRECTORY
+if  [ -d ".repo" ] && [ -d "vendor/orion" ] ; then
+	printf "\n$blu Setting ORION_HOME to `pwd` $end\n"
+	ORION_HOME=pwd
+else
+	printf "\n$red execute the script from home directory $end"
+	exit 1
+fi
+#=========================================
+
 
 if [ "$2" == "" ]; then			#No device name
 	
-	echo "No device name entered. Select from the foll-"
-	cat  $ORION_DEVICES
+	printf "\n$red No device name entered.$end $grn Select from the foll:\n$end"
+	cat  `pwd`/vendor/orion/orion.devices
 	
 elif [ "$2" != "" ]; then		#Device name given. Check flags
 
 	while getopts "s:f:r:c:p:d" opt; do
 		case $opt in 
 			s)					# sync
-				echo "Syncing repo"
+				printf "\n$blu Syncing repo$end\n"
 				eval "repo sync -c --force-sync --force-broken --no-clone-bundle"
-				echo "Repo sync done"
+				printf "\n$blu Repo sync done$end\n"
 				;;
 				
 			f)					# force sync 
-				echo "Force syncing repo"
+				printf "\n$blu Force syncing repo$end\n"
 				eval "repo sync -c --force-sync --force-broken --no-clone-bundle"
-				echo "Repo sync done"
+				printf "\n$blu Repo sync done$end\n"
 				;;
 			
 			r)				#Fixed force sync 
-				echo "Fixing broken sync and syncing"
-				echo "this step will remove your .repo/repo folder and resync repo projects. Continue (y/n)?"
+				printf "\n$blu Fixing broken sync and syncing\n$end"
+				printf " $red This step will remove your .repo/repo folder and resync repo projects. Continue (y/n)?$end"
 				read ch
 				if [ "$ch" == "y" ]; then
-					echo "removing .repo/repo"
+					printf "$blu removing .repo/repo\n$end"
 					rm -rf ".repo/repo"
 					eval "repo init -u git://github.com/TeamOrion/platform_manifest.git -b mm6.0"
 					eval "repo sync -c --force-sync --force-broken --no-clone-bundle"
@@ -60,9 +66,9 @@ elif [ "$2" != "" ]; then		#Device name given. Check flags
 
 	
 				elif [ "$ch" == "n" ]; then
-					echo "Aborted"
+					printf "$yel Aborted$end"
 				else
-					echo "Wrong option. Aborted"
+					echo "$red Wrong option. Aborted$end"
 				fi
 				;;
 			
@@ -71,20 +77,23 @@ elif [ "$2" != "" ]; then		#Device name given. Check flags
 				;;
 			
 			p)				#clean prebuilt
-				echo "clearing prebuilt/chromium"
-				rm -rf "$ORION_HOME/prebuilts/chromium/$2"
+				printf "\n$blu clearing prebuilt/chromium$end\n"
+				rm -rf  prebuilts/chromium/$2
 				;;
 				
 			d)				#default
-				echo "building dirty (default)"
-				echo "removing build.prop of older build"
-				rm -rf "$ORION_HOME/out/target/product/$2/system/build.prop"
-				rm -rf "$ORION_HOME/out/target/product/$2/system/*.zip"
+				printf "$blu building dirty (default)$end\n"
+				if [ -f "`$ORION_HOME`/out/target/product/$2/system/build.prop" ]; then
+					printf "$yel removing build.prop of older build\n$end"
+					rm  `$ORION_HOME`/out/target/product/$2/system/build.prop				
+					rm  `$ORION_HOME`/out/target/product/$2/*.zip
+					rm  `$ORION_HOME`/out/target/product/$2/*.zip.md5sum
+				fi
 				;;
 			
 			\?) 
-				echo "Usage ./orion.sh -[s][f][r][c][p][d] [device name]"
-				exit 1;
+				echo "$yel Usage ./orion.sh -[s][f][r][c][p][d] [device name]$end"
+				exit 1
 				;;
 				
 		esac
@@ -93,17 +102,18 @@ elif [ "$2" != "" ]; then		#Device name given. Check flags
 	eval ". build/envsetup.sh"
 	export USE_PREBUILT_CHROMIUM=1
 	#Check official support
-		if grep -Fxq $2 $ORION_DEVICES
+		if grep -Fxq $2 `pwd`/vendor/orion/orion.devices
 			then
-				echo "====$2 is officially supported===="
+				
+				printf "\n$blu $2 is officially supported$end\n"
 				export ORION_RELEASE=true
 			else
-				echo "device is not supported officially; building unofficially"
+				printf "$grn\ndevice is not supported officially; building unofficially$end\n"
 		fi
 		
 	# compile
 	eval "brunch $2"
 			
 else 
-	echo "Error"
+	echo "$red Error$end"
 fi
