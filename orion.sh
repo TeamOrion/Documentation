1144ce4a7fc7c2292b672a48872f40976945976a
SohamJ <soham4jam@gmail.com>
2015-12-09 15:22:13 -0500

    removing zips on dirty build
    Change-Id: I36ac095b64be46b857d92c64e04342d4ad74b8db

--- orion.sh
+++ orion.sh
@@ -84,7 +84,7 @@ elif [ "$2" != "" ]; then		#Device name given. Check flags
 			d)				#default
 				printf "$blu building dirty (default)$end\n"
 				if [ -f "`$ORION_HOME`/out/target/product/$2/system/build.prop" ]; then
-					printf "$yel removing build.prop of older build\n$end"
+					printf "$yel rm build.prop, *.zip\n$end"
 					rm  `$ORION_HOME`/out/target/product/$2/system/build.prop				
 					rm  `$ORION_HOME`/out/target/product/$2/*.zip
 					rm  `$ORION_HOME`/out/target/product/$2/*.zip.md5sum
