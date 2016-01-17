e7705ad630e61c6dbe9bcf80a2fb7bb4aa9bec84
Akhil Narang <akhil.narang@protonmail.com>
2016-01-17 20:46:35 +0530

    orion: update build script for MM, remove sync flags
    The flags are added in manifest by default

--- orion.sh
+++ orion.sh
@@ -9,7 +9,6 @@
 # s = sync | f= force sync | r=repair sync
 # c = clean 
 # d = default (dirty)
-# p = clear prebuilt chromium
 #=========================================
 
 #COLORS -
@@ -24,7 +23,7 @@ end=$'\e[0m'
 #SET THE HOME DIRECTORY
 if  [ -d ".repo" ] && [ -d "vendor/orion" ] ; then
 	printf "\n$blu Setting ORION_HOME to `pwd` $end\n"
-	ORION_HOME=pwd
+	ORION_HOME=$(pwd)
 else
 	printf "\n$red execute the script from home directory $end"
 	exit 1
@@ -35,21 +34,21 @@ fi
 if [ "$2" == "" ]; then			#No device name
 	
 	printf "\n$red No device name entered.$end $grn Select from the foll:\n$end"
-	cat  `pwd`/vendor/orion/orion.devices
+	cat  $ORION_HOME/vendor/orion/orion.devices
 	
 elif [ "$2" != "" ]; then		#Device name given. Check flags
 
-	while getopts "s:f:r:c:p:d" opt; do
+	while getopts "s:f:r:c:d" opt; do
 		case $opt in 
 			s)					# sync
 				printf "\n$blu Syncing repo$end\n"
-				eval "repo sync -c --force-sync --force-broken --no-clone-bundle"
+				eval "repo sync --force-sync --no-clone-bundle"
 				printf "\n$blu Repo sync done$end\n"
 				;;
 				
 			f)					# force sync 
 				printf "\n$blu Force syncing repo$end\n"
-				eval "repo sync -c --force-sync --force-broken --no-clone-bundle"
+				eval "repo sync --force-sync --no-clone-bundle"
 				printf "\n$blu Repo sync done$end\n"
 				;;
 			
@@ -61,7 +60,7 @@ elif [ "$2" != "" ]; then		#Device name given. Check flags
 					printf "$blu removing .repo/repo\n$end"
 					rm -rf ".repo/repo"
 					eval "repo init -u git://github.com/TeamOrion/platform_manifest.git -b 6.0.1"
-					eval "repo sync -c --force-sync --force-broken --no-clone-bundle"
+					eval "repo sync --force-sync --no-clone-bundle"
 
 
 	
@@ -76,11 +75,6 @@ elif [ "$2" != "" ]; then		#Device name given. Check flags
 				make clean
 				;;
 			
-			p)				#clean prebuilt
-				printf "\n$blu clearing prebuilt/chromium$end\n"
-				rm -rf  prebuilts/chromium/$2
-				;;
-				
 			d)				#default
 				printf "$blu building dirty (default)$end\n"
 				if [ -f "`$ORION_HOME`/out/target/product/$2/system/build.prop" ]; then
@@ -92,7 +86,7 @@ elif [ "$2" != "" ]; then		#Device name given. Check flags
 				;;
 			
 			\?) 
-				echo "$yel Usage ./orion.sh -[s][f][r][c][p][d] [device name]$end"
+				echo "$yel Usage ./orion.sh -[s][f][r][c][d] [device name]$end"
 				exit 1
 				;;
 				
@@ -100,7 +94,6 @@ elif [ "$2" != "" ]; then		#Device name given. Check flags
 	done
 	
 	eval ". build/envsetup.sh"
-	export USE_PREBUILT_CHROMIUM=1
 	#Check official support
 		if grep -Fxq $2 `pwd`/vendor/orion/orion.devices
 			then
