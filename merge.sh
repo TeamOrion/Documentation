5e2d62d4234eada20006f4be2188ffd4b08f2dd6
Akhil Narang <akhil@akhilnarang.me>
2016-10-09 01:02:18 +0530

    merge.sh: fix, add some comments
    Signed-off-by: Akhil Narang <akhil@akhilnarang.me>

--- merge.sh
+++ merge.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+
 # Assumes source is in users home in a directory called "orion"
 export ORIONPATH="${HOME}/orion"
+
+# Set the tag you want to merge
 export TAG="android-6.0.1_r72"
 
+# Orion manifest is setup with path first, then repo name, so the path attribute is after 2 spaces, and the name within "" in it
 for repos in $(grep 'remote="orion"' ${ORIONPATH}/.repo/manifests/default.xml  | awk '{print $2}' | cut -d '"' -f2)
 do
-export path=$pwd;
 cd $repos;
 git fetch https://android.googlesource.com/platform/$repos && git merge $TAG;
 if [ $? -ne 0 ];
@@ -14,5 +17,6 @@ echo "$repos" >> failed
 else
 echo "$repos" >> success
 fi
-cd $path ; done
+cd ${ORIONPATH};
+done
 
