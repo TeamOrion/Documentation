fd688afbcfdfdfa4b4c3eec10a3000df618ca294
Akhil Narang <akhil@akhilnarang.me>
2016-10-09 15:28:48 +0530

    merge.sh: be a bit more verbose
    Signed-off-by: Akhil Narang <akhil@akhilnarang.me>

--- merge.sh
+++ merge.sh
@@ -9,18 +9,26 @@ export TAG="android-6.0.1_r72"
 # Orion manifest is setup with path first, then repo name, so the path attribute is after 2 spaces, and the name within "" in it
 for repos in $(grep 'remote="orion"' ${ORIONPATH}/.repo/manifests/default.xml  | awk '{print $2}' | cut -d '"' -f2)
 do
+echo -e ""
+echo "Merging $repos"
+echo -e ""
 cd $repos;
 git fetch https://android.googlesource.com/platform/$repos -qt && git merge $TAG;
 if [ $? -ne 0 ];
 then
 echo "$repos" >> ${ORIONPATH}/failed
+echo "$repos failed :C"
 else
 echo "$repos" >> ${ORIONPATH}/success
+echo "$repos succeeded"
 fi
+echo -e ""
 cd ${ORIONPATH};
 done
 
+echo -e ""
 echo -e "These repos failed"
 cat ./failed
+echo -e ""
 echo -e "These succeeded"
 cat ./success
