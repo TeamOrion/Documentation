27037a66640dfec4192e8c1a1f82b3d6360c73a4
Akhil Narang <akhil@akhilnarang.me>
2016-10-09 15:20:39 +0530

    merge.sh: fetch tags and fetch quietly
    Signed-off-by: Akhil Narang <akhil@akhilnarang.me>

--- merge.sh
+++ merge.sh
@@ -10,7 +10,7 @@ export TAG="android-6.0.1_r72"
 for repos in $(grep 'remote="orion"' ${ORIONPATH}/.repo/manifests/default.xml  | awk '{print $2}' | cut -d '"' -f2)
 do
 cd $repos;
-git fetch https://android.googlesource.com/platform/$repos && git merge $TAG;
+git fetch https://android.googlesource.com/platform/$repos -qt && git merge $TAG;
 if [ $? -ne 0 ];
 then
 echo "$repos" >> ${ORIONPATH}/failed
