3bcd9b9d7529209a4eefb2aedfca39cebe01cd8b
Akhil Narang <akhil@akhilnarang.me>
2016-10-11 19:14:48 +0530

    merge.sh: final underp
    Signed-off-by: Akhil Narang <akhil@akhilnarang.me>

--- merge.sh
+++ merge.sh
@@ -13,7 +13,7 @@ export ORIONPATH="${HOME}/orion"
 # Set the tag you want to merge
 export TAG="android-6.0.1_r72"
 
-do_not_merge="vendor/orion external/chromium-webview"
+do_not_merge="vendor/orion,external/chromium-webview"
 # Orion manifest is setup with path first, then repo name, so the path attribute is after 2 spaces, and the name within "" in it
 for repos in $(grep 'remote="orion"' ${ORIONPATH}/.repo/manifests/default.xml  | awk '{print $2}' | cut -d '"' -f2)
 do
